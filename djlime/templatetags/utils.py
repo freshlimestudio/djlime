365263aec0b65ab515b15d6a0c600470273f6294
Andrey Voronov <voronov84@gmail.com>
2012-02-28 16:41:55 +0200

    Added is_current_link template tag

---
+++ djlime/templatetags/utils.py
@@ -0,0 +1,21 @@
+import re
+
+from django.template import Library
+
+register = Library()
+
+
+@register.simple_tag
+def is_current_link(request, pattern, css_class='current'):
+    """
+    Searches regexp template in request.path
+
+    :param request: request instance
+    :param pattern: valid regexp string
+    :param css_class: optional css class value
+    :returns: css_class value
+    :rtype: string
+    """
+    if re.search(pattern, request.path):
+        return css_class
+    return ''
