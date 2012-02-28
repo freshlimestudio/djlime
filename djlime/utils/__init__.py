ef5a9363283193613b7158ab4522a4eddba4620f
Andrey Voronov <voronov84@gmail.com>
2012-02-28 12:58:43 +0200

    Added utils package

---
+++ djlime/utils/__init__.py
@@ -0,0 +1,11 @@
+import os
+import uuid
+
+
+def get_file_path(obj, filename):
+    if hasattr(obj, 'upload_dir'):
+        extension = filename.split('.')[-1]
+        filename = "%s.%s" % (uuid.uuid4(), extension)
+        return os.path.join(obj.upload_dir, filename)
+    else:
+        raise AttributeError("%s does not have 'upload_dir' attribute" % obj.__class__.__name__)
