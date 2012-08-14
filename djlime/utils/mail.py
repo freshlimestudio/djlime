8db4f636a3d4a9e505de8a9abfe86eaf06910d4b
Andrey Voronov <voronov84@gmail.com>
2012-08-14 12:41:58 +0300

    Fixed params bug in send_mail

--- djlime/utils/mail.py
+++ djlime/utils/mail.py
@@ -14,13 +14,14 @@ from django.template.loader import render_to_string
 from django.core.mail.message import EmailMultiAlternatives
 
 
-def send_mail(recipients=None, sender=None, subject_template='', html_template=None,
-              text_template=None, context=None):
+def send_mail(recipients=None, subject_template='', html_template=None,
+              text_template=None, context=None, **kwargs):
     subject = render_to_string(subject_template, context)
     # Email subject *must not* contain newlines
     subject = ''.join(subject.splitlines())
     html_msg = render_to_string(html_template, context)
     text_msg = render_to_string(text_template, context)
+    sender = kwargs.pop('sender', None)
     if not isinstance(recipients, list):
         recipients = [recipients]
     msg = EmailMultiAlternatives(subject, text_msg,
