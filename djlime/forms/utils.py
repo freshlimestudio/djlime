b6418d376be997eed91f86eb5793b7bbbdc27e69
eyvoro <voronov84@gmail.com>
2013-01-24 10:14:29 +0200

    Revert new functionality

--- djlime/forms/utils.py
+++ djlime/forms/utils.py
@@ -11,15 +11,6 @@
 from django.utils import simplejson
 
 
-def _errors_to_dict(form):
-    return dict([(k, v[0]) for k, v in form.errors.items()])
-
-
 def form_errors_to_json(form):
-    if hasattr(form, '__iter__'):
-        rv = {'success': False, 'errors': []}
-        for f in form:
-            rv['errors'].append(_errors_to_dict(f))
-    else:
-        rv = {'success': False, 'errors': _errors_to_dict(form)}
-    return simplejson.dumps(rv)
+    errors = dict([(k, v[0]) for k, v in form.errors.items()])
+    return simplejson.dumps({'success': False, 'errors': errors})
