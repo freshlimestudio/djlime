26fb76fe3c682a052496c885bbc0e7ea8590a4a8
Andrey Voronov <voronov84@gmail.com>
2013-10-28 14:03:15 +0200

    Release 0.0.26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djlime',
-    version='0.0.25',
+    version='0.0.26',
     description='Django goodies.',
     author='Andrey Voronov',
     author_email='voronov84@gmail.com',
