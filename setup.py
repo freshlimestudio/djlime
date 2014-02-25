3d1dccdeaf0e659f3a39f405daaced92e1826bfb
Andrey Voronov <voronov84@gmail.com>
2014-02-25 17:32:01 +0200

    Release v0.0.32

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djlime',
-    version='0.0.31',
+    version='0.0.32',
     description='Django goodies.',
     author='Andrey Voronov',
     author_email='voronov84@gmail.com',
